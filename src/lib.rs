830f70e59702b28addd28f2fbb7dcef1ccd85efc
Manuel Reich <mr@wundf.net>
2024-05-20 22:38:30 +0200

    Start playing around with replacing expressions

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,60 @@
 use swc_core::{
-    common::Spanned,
+    common::util::take::Take,
     ecma::{
         ast::*,
         transforms::testing::test_inline,
         visit::{VisitMut, VisitMutWith},
     },
 };
+use tracing::debug;
 
-pub struct TransformVisitor;
+const IMPORT_NAME: &str = "errnesto/eszett";
+
+#[derive(Default)]
+pub struct TransformVisitor {
+    sz_identifier: Option<Ident>,
+}
 
 impl VisitMut for TransformVisitor {
-    fn visit_mut_bin_expr(&mut self, e: &mut BinExpr) {
-        e.visit_mut_children_with(self);
+    fn visit_mut_import_decl(&mut self, n: &mut ImportDecl) {
+        n.visit_mut_children_with(self);
 
-        if e.op == op!("===") {
-            e.left = Ident::new("kdy1".into(), e.left.span()).into();
+        if n.src.value == IMPORT_NAME {
+            // store identifier
+            for specifier in &n.specifiers {
+                if let ImportSpecifier::Default(s) = specifier {
+                    self.sz_identifier = Some(s.local.clone())
+                }
+            }
+            // convert import to an invalid value
+            n.take();
+        }
+    }
+    fn visit_mut_module_items(&mut self, stmts: &mut Vec<ModuleItem>) {
+        stmts.visit_mut_children_with(self);
+
+        // remove invalid imports
+        stmts.retain(|s| {
+            if let ModuleItem::ModuleDecl(ModuleDecl::Import(x)) = s {
+                return !x.src.is_empty();
+            }
+            return true;
+        });
+    }
+
+    fn visit_mut_expr(&mut self, n: &mut Expr) {
+        n.visit_mut_children_with(self);
+
+        let sz_identifier;
+        debug!("{:?}", n);
+        match &self.sz_identifier {
+            Some(sz) => sz_identifier = sz,
+            _ => return,
+        }
+
+        if let Some(t) = n.as_tagged_tpl() {
+            let expr = Expr::Ident(sz_identifier.clone());
+            n.take();
         }
     }
 }
@@ -23,8 +63,13 @@ impl VisitMut for TransformVisitor {
 use swc_core::ecma::visit::as_folder;
 test_inline!(
     Default::default(),
-    |_| as_folder(TransformVisitor),
+    |_| as_folder(TransformVisitor::default()),
     boo,
-    r#"foo === bar;"#,
-    r#"kdy1 === bar;"#
+    r#"
+        import sz from 'errnesto/eszett'
+        const hui = sz`my-class`
+    "#,
+    r#"
+        const hui = sz
+    "#
 );
