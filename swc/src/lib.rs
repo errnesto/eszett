bf48bbe6f8391a79e77937f3a8e148226a801d40
Manuel Reich <mr@wundf.net>
2024-08-18 12:57:05 +0200

    Print project meta during transform

--- swc/src/lib.rs
+++ swc/src/lib.rs
@@ -15,11 +15,33 @@ pub fn transformer(filepath: impl Into<String>) -> impl VisitMut {
 }
 
 #[plugin_transform]
-pub fn process_transform(mut program: Program, data: TransformPluginProgramMetadata) -> Program {
-    let filepath = match data.get_context(&TransformPluginMetadataContextKind::Filename) {
+pub fn process_transform(
+    mut program: Program,
+    metadata: TransformPluginProgramMetadata,
+) -> Program {
+    let filepath = match metadata.get_context(&TransformPluginMetadataContextKind::Filename) {
         Some(s) => s,
         None => String::from(""),
     };
+    let config = metadata
+        .get_transform_plugin_config();
+        // .get("root")
+        // .and_then(|value| value.as_str())
+        // .map(PathBuf::from)
+        // .unwrap_or_else(|| {
+        //     // Default to the current directory if not specified
+        //     std::env::current_dir().expect("Failed to get current directory")
+        // });
+
+    println!("------------------------");
+    println!("{:?}", config);
+    println!("------------------------");
+
+    // let relative_path = match filepath.strip_prefix(&project_root) {
+    //     Some(s) => s,
+    //     None => &filepath,
+    // };
+
     program.visit_mut_with(&mut transformer(filepath));
 
     return program;
